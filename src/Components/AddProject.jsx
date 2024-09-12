4a30ba39a91c0f198042b5aed3e412224932cbd9
antonia trombetta <antonia.dorazio@gmail.com>
2024-09-12 18:48:06 +0200

    fixing deploy for netlify

--- src/Components/AddProject.jsx
+++ src/Components/AddProject.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import styles from './Addproject.module.css';  
+import styles from './AddProject.module.css';  
 
 
 function AddProject() {
